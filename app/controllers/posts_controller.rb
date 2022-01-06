729bfceb83b05f4bd23beb9cf3adf1a837049d5d
osyoyu <osyoyu@osyoyu.com>
2022-01-06 11:02:35 +0900

    Impl post#new, post#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_url_key!(params[:url_key])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      title: params[:post][:title],
+      body: params[:post][:body],
+      is_draft: params[:post][:is_draft],
+    )
+    @post.save! # TODO: Error handling
+    redirect_to @post
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "posts#index"
 
+  resources :posts, only: [:new, :create]
   resources :posts, only: [:show], param: :url_key
 end
